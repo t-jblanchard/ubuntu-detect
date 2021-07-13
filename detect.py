fc1d31e4aeb76ad4849a9b26176120a2d2e5f400
Julia Blanchard <t-jblanchard@microsoft.com>
2021-07-13 10:18:39 -0700

    Multiprocessing Setup

--- detect.py
+++ detect.py
@@ -6,13 +6,35 @@ import sqlite3
 import multiprocessing 
 from functools import partial 
 
+manager = multiprocessing.Manager()
+files = manager.dict()
+
 # usage function 
 def usage():
     sys.exit(0)
 
+# function to add information to the database 
+def addto_db(path, command, line_num):
+    # make sure file path is a key in data, if not add
+    if path not in files:
+        files[path] = {}
+    
+    # check if command is a key for data[path]
+    if command in files[path].keys():
+        # apend line_num to list 
+        files[path][command].append(line_num)
+        # ensure no duplicates of line numbers
+        set(files[path][command])
+    # if not in data[path] add
+    else:
+        files[path][command] = [line_num]
+
+    return True 
+
+
 # function to scan line for specific package names and extensions
 def package_scan(package, line):
-    
+
     return None 
 
 
@@ -23,16 +45,16 @@ def filepath_scan(filepath, line):
 
 
 # function to scan line for specfic commands 
-def command_scan(command, line):
+def command_scan(path, line_num, line, command):
     # put in proper format 
-    c = command[0] 
+    c = str(command[0])
     l = str(line)
 
     # check if the command is in the line
     if c in l:
-        return c
+        addto_db(path, c, line_num)
 
-    return ''
+    return files
 
 
 # function to scan line in a file
@@ -47,15 +69,17 @@ def line_scan(path, line, line_num):
     inputs = cur.fetchall()
     records = len(inputs)
 
-    # start processing pool, with a process for each record 
+    # start processing pool, with a process for each record
     pool = multiprocessing.Pool()
     pool = multiprocessing.Pool(processes=records)
 
     # set line arg for these command_scan function calls 
-    func_command = partial(command_scan, line=line)
+    #func_command = partial 
+    func_command = partial(command_scan, path, line_num, line)
 
     # do processing 
-    found_commands = pool.map(func_command, inputs)
+    files = pool.map(func_command, inputs)
+
     #if found_commands != ['']:
         #print(found_commands)
 
@@ -98,9 +122,17 @@ def file_scan(path):
             
     return True
 
+# function to scan directory name for Ubuntu-specifics
+def dirname_scan(dir_path):
+    return True
+
+
 
 # function to scan directories for files 
 def dir_scan(cwd): 
+    # scan directory name for Ubuntu-specifics 
+    dirname_scan(cwd)
+
     # get items in directory 
     dir_items = os.listdir(cwd)
 
@@ -130,6 +162,7 @@ def dir_scan(cwd):
 
 
 def main():
+    global files
     # if singular file inputted (argv = 2), call scan_file
     if len(sys.argv) == 2:
         file_scan(str(sys.argv[1]))
@@ -139,10 +172,12 @@ def main():
         # get the current working directory path and call scan_dir 
         cwd_path = os.getcwd()
         dir_scan(cwd_path) 
-        
+
     # call usage if argv > 2 
     else:
         usage()
+    
+    print(files)
 
 
 if __name__ == '__main__':
